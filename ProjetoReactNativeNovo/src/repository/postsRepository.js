7d5624104b88b669a03858cadc51369ff6e59f1c
Junior Monteiro <jmjuniormonteiro@gmail.com>
2021-07-10 12:05:29 -0300

    Criando o FlatList

--- ProjetoReactNativeNovo/src/repository/postsRepository.js
+++ ProjetoReactNativeNovo/src/repository/postsRepository.js
@@ -1,7 +1,14 @@
 import api from '../services/api';
 
-const getPosts = () => {
-    
+const getPosts = async () => {
+    try {
+        //Executar ação
+        const postList = await api.get('posts', {responseType: 'text'})
+        if(postList.status !== 200) throw new Error("DEU RUIM")
+        return postList.data
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 export default getPosts;
--- ProjetoReactNativeNovo/src/screens/Posts/index.js
+++ ProjetoReactNativeNovo/src/screens/Posts/index.js
@@ -1,16 +1,51 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList, ActivityIndicator } from 'react-native';
 
 import styles from './styles';
 
 import getPosts from '../../repository/postsRepository';
 
 const Posts = () => {
+    const [postList, setPostList] = useState([]);
+    const [loading, setLoading] = useState(false)
+
+    const carregaPosts = async () => {
+        setLoading(true)
+        const posts = await getPosts()
+        setPostList(posts)
+        setLoading(false)
+    }
+
+    const renderFooter = () => {
+        if (!loading) return null
+        return <ActivityIndicator size="large" color="#20C0DF" />
+    }
+
+    useEffect(() => {
+        carregaPosts()
+    }, [])
 
     return (
-        <View>
-            <Text>Post Screen</Text>
-        </View>
+        <FlatList
+            style={styles.container}
+            data={postList}
+            keyExtractor={item => item.id}
+            ListFooterComponent={renderFooter}
+            renderItem={({ item }) => (
+                <View
+                    style={{
+                        backgroundColor: 'lightgreen',
+                        margin: 5,
+                        padding: 20,
+                        borderRadius: 12,
+                    }}>
+                    <Text style={{ fontSize: 18, textAlign: 'center' }}>
+                        {item.title.toUpperCase()}
+                    </Text>
+                </View>
+
+            )}
+        />
     )
 }
 
