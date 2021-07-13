8c0e3babe88423c9a1a1a6fc317b64cd211dfb1a
Junior Monteiro <jmjuniormonteiro@gmail.com>
2021-07-13 19:12:22 -0300

    Finalizando projeto

--- ProjetoReactNativeNovo/src/screens/Posts/index.js
+++ ProjetoReactNativeNovo/src/screens/Posts/index.js
@@ -1,20 +1,58 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, ActivityIndicator } from 'react-native';
+import { View, Text, FlatList, ActivityIndicator, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
 
 import getPosts from '../../repository/postsRepository';
+
+import getRealm from '../../services/realm';
+
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const Posts = () => {
     const [postList, setPostList] = useState([]);
     const [loading, setLoading] = useState(false)
 
     const carregaPosts = async () => {
-        setLoading(true)
-        const posts = await getPosts()
-        setPostList(posts)
-        setLoading(false)
+        setLoading(true);
+        const posts = await getPosts();
+        setPostList(posts);
+        setLoading(false);
+    };
+
+    const savePost = async (post) => {
+        const realm = await getRealm();
+        try{
+            realm.write(() => {
+                realm.create('Post', {
+                    id: post.id,
+                    userId: post.userId,        
+                    title: post.title,
+                    body: post.body,
+                });
+                console.log("Post armazenado com Sucesso!");
+            })
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const deleteRealm = async () => {
+        const realm = await getRealm();
+
+        realm.write(() => {
+            const realmPosts = realm.objects("Post");
+            realm.delete(realmPosts);
+        })
+        console.log("Demitido com sucesso!")
     }
+
+    const getPostsRealm = async () => {
+        const realm = await getRealm();
+        const realmPosts = realm.objects("Post");
+        const filter = realmPosts.filtered('id = 2')
+        console.log(filter);
+    };
 
     const renderFooter = () => {
         if (!loading) return null
@@ -22,7 +60,9 @@ const Posts = () => {
     }
 
     useEffect(() => {
-        carregaPosts()
+        carregaPosts();
+        getPostsRealm();
+        deleteRealm();
     }, [])
 
     return (
@@ -38,10 +78,16 @@ const Posts = () => {
                         margin: 5,
                         padding: 20,
                         borderRadius: 12,
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
                     }}>
-                    <Text style={{ fontSize: 18, textAlign: 'center' }}>
+                    <Text style={{ fontSize: 18, textAlign: 'center', width: '80%'}}>
                         {item.title.toUpperCase()}
                     </Text>
+                    <TouchableOpacity onPress={() => savePost(item)}>
+                        <Icon name="save" size={32} />
+                    </TouchableOpacity>
                 </View>
 
             )}
